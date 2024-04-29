30ee05af4491e679c63213396b94e1163259ad23
Saulius Vincevičius <sauliusvince@gmail.com>
2024-04-30 00:22:44 +0100

    Fixed bugs

--- project_manager.sh
+++ project_manager.sh
@@ -2,10 +2,10 @@
 
 # Variables
 script_dir=$(dirname "$0")
-projects_dir="$script_dir/projects.txt"
+projects_dir="$script_dir/projects_list.save"
 
 # Cheack if file exists
-if [ ! -e $projects_dir ]; then
+if [ ! -e "$projects_dir" ]; then
     echo "Init Project Manager!"
     touch $projects_dir
 fi
@@ -14,10 +14,13 @@ fi
 if [[ $# -eq 1 && $1 == "add" ]]
 then
     # Check if project exists
-    if ! grep -q "$(pwd)" $projects_dir
+    if ! grep -q "$(pwd)" "$projects_dir"
     then
+        cat "$projects_dir" > temp
+        pwd > "$projects_dir"
+        cat temp >> "$projects_dir"
+        rm temp
         # Add current directory
-        pwd >> $projects_dir
         echo "Project Added!"
     else
         echo "Can't add project already exists"
@@ -30,8 +33,8 @@ fi
 if [[ $# -eq 1 && $1 == "remove" ]]
 then
     # Add current directory
-    grep -v "$(pwd)" $projects_dir > temp
-    mv temp $projects_dir
+    grep -v "$(pwd)" "$projects_dir" > temp
+    mv temp "$projects_dir"
     echo "Project Removed!"
 
     exit 0
@@ -40,16 +43,25 @@ fi
 # If no arguments then list the projects
 if [[ $# -eq 0 ]]
 then
-    selected=`cat $projects_dir | fzf`
+    selected=`cat "$projects_dir" | fzf`
     selected_name=$(basename "$selected" | tr . _)
    
-    if [[ $selected == "q" || $selected == "quit" ]]; then
-        exit 0
-    fi
-
     if [ -z $selected_name ]; then
         exit 0
     fi
+   
+    # Exit if the selection is not project
+    if ! grep -q "$selected" "$projects_dir"; then
+        echo "Invalid project selection"
+        exit 0
+    fi
+
+    # put in the front
+    grep -v "$selected" "$projects_dir" > temp
+    echo "$selected" > "$projects_dir"
+    cat temp >> "$projects_dir"
+    rm temp
+    echo "Selected"
 
     # Check if tmux is running
     if [[ -z $TMUX ]] && [[ -z $tmux_running ]]
@@ -57,7 +69,7 @@ then
         # Check if session with name exists
         if ! tmux has-session -t=$selected_name 2> /dev/null; then
             echo "Session not found, creating a new one"
-            tmux new-session -s $selected_name -c $selected
+            tmux new-session -s $selected_name -c "$selected"
         else
             echo "Session found, attaching"
             tmux attach -t $selected_name
@@ -69,7 +81,7 @@ then
     # Check if session with name exists
     if ! tmux has-session -t=$selected_name 2> /dev/null; then
         echo "Session was not found, creating a session"
-        tmux new-session -ds $selected_name -c $selected
+        tmux new-session -ds $selected_name -c "$selected"
     fi
 
     tmux switch-client -t $selected_name
